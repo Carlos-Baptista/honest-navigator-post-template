ca219d7a192e4464e4ae70f8a8d534278056cd41
Carlos Baptista <85baptista@gmail.com>
2026-09-08 21:59:56 +0100

    new version

---
+++ sw.js
@@ -0,0 +1,50 @@
+// Sobe este número sempre que fizeres uma alteração ao ficheiro,
+// para forçar o telemóvel a ir buscar a versão nova em vez de usar a antiga em cache.
+const CACHE_VERSION = 'editor-capas-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './fonts/awesome-serif.ttf',
+  './fonts/anybody.ttf',
+  './icons/icon-120.png',
+  './icons/icon-152.png',
+  './icons/icon-167.png',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request)
+        .then((response) => {
+          if (response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
